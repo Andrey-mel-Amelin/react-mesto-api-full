02c8a751724bcecf25d81abd0764d6d55b9e5ee8
Амелин Андрей Александрович <amelin_a_qschool@qsoft.ru>
2023-10-08 21:15:48 +0300

    fix: исправь порт

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const routes = require('./routes');
 const centralizedErrorHandler = require('./middlewares/centralizedErrorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3002, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
